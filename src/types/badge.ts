c7ced5e12d41b67f82fa96bd8ab5b9a808b9f397
Luismour <luismsantos.contato@gmail.com>
2026-06-03 18:50:49 -0300

    add: badge-type

---
+++ src/types/badge.ts
@@ -0,0 +1,19 @@
+export type Branch = "Lobinho" | "Escoteiro" | "Sênior" | "Pioneiro" | "Escotista";
+
+export interface BadgeDesign {
+  baseShape: string; 
+  baseColor: string; 
+  symbol: string; 
+  symbolPosition: string; 
+  borderColor: string; 
+}
+
+export interface Badge {
+  id: string; 
+  ownerName: string;
+  scoutGroup: string;
+  region: string;
+  branch: Branch;
+  badgeDesign: BadgeDesign;
+  collectedAt?: number;
+}
